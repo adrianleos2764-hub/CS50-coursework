06093ca17277ad803e483a4acd9b0daf9848c1dc
Adrian Leos <adrianleos2764@gmail.com>
2026-06-27 15:50:20 -0400

    Add files via upload

---
+++ CS50 set 0/indoor.py
@@ -0,0 +1,4 @@
+# grab a users input and return it in lowercase as if the user is yelling
+response = input("How are you? ") #ask the use for a response
+print(response.lower()) # prints repsonse in lowercase
+print("That's how you use an indoor voice!")
