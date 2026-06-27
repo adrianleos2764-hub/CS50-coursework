085078b5c7590997fef1f360effc5bcdeab45c2c
Adrian Leos <adrianleos2764@gmail.com>
2026-06-27 15:55:36 -0400

    Delete playback.py

--- playback.py
+++
@@ -1,3 +0,0 @@
-# takes desired user input and "slows it down" by adding 3 dots inbetween each word
-response = input("whats the source? ") #ask the user for a source to slow down
-print(response.replace(" ", "...")) # prints the response with 3 dots inbetween each word
